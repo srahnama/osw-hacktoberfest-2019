723a9c10750ef76fd100ea6cc152c4071e1c7902
PseudoNerd <tkb9721@gmail.com>
2019-10-01 04:17:15 +0530

    added requested changes into the pre existing PR

--- src/components/FooterBase.vue
+++ src/components/FooterBase.vue
@@ -3,7 +3,9 @@
     <div class="container">
       <div class="py-4">
         <p class="m-0 text-center">
-          <a href="https://github.com/OSWeekends/osw-hacktoberfest-2019"><i class="fab fa-github fa-2x" href="https://github.com/OSWeekends/osw-hacktoberfest-2019" ></i></a>
+          <a href="https://github.com/OSWeekends/osw-hacktoberfest-2019"><i class="fab fa-github fa-2x"></i></a>
+          <a href="https://github.com/OSWeekends/osw-hacktoberfest-2019"><i class="fab fa-twitter fa-2x"></i></a>
+          <a href="https://github.com/OSWeekends/osw-hacktoberfest-2019"><i class="fab fa-meetup fa-2x"></i></a>
           </p>
       </div>
     </div>
@@ -16,9 +18,24 @@ export default {
 </script>
 
 <style lang="stylus">
+
 .footer
   position fixed
   bottom 0
   width 100%
 
+i
+  margin 2%
+
+.fab
+  shadows : 10px 10px #000
+
+.fa-github
+  color #242a2e
+
+.fa-twitter
+  color #4fa1f2
+
+.fa-meetup
+  color #ee4060
 </style>

60eeaed71073e8d49517964d71efef09431e66bf
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-10-02 15:16:05 +0200

    WIP stats page

--- src/views/Stats/Index.vue
+++ src/views/Stats/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="stats">
     <h1 class="text-center">Stats Page</h1>
   </div>
 </template>
@@ -8,16 +8,17 @@
 export default {
   name: 'Stats',
   methods: {
-    // hexToRGB (hex) {
-    //   // Delete '#'
-    //   if (hex.charAt(0) === '#') {
-    //     hex = hex.substr(1)
-    //   }
-    //   const r = parseInt(hex.substring(0, 2), 16)
-    //   const g = parseInt(hex.substring(2, 4), 16)
-    //   const b = parseInt(hex.substring(4, 6), 16)
-    //   this.colorRGB = `rgba(${r}, ${g}, ${b}, 1)`
-    // }
+    hexToRGB (hex) {
+      // Delete '#'
+      if (hex.charAt(0) === '#') {
+        hex = hex.substr(1)
+      }
+      const r = parseInt(hex.substring(0, 2), 16)
+      const g = parseInt(hex.substring(2, 4), 16)
+      const b = parseInt(hex.substring(4, 6), 16)
+      const colorRGB = `rgba(${r}, ${g}, ${b}, 1)`
+      console.log(colorRGB)
+    }
   }
 }
 </script>

9d160d50a0e381e636f266162dbac068326406d8
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-10-02 20:16:34 +0200

    Twitter btn

--- src/components/ShareTwitterBtn.vue
+++ src/components/ShareTwitterBtn.vue
@@ -1,12 +1,26 @@
 <template>
-  <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large"
-     data-text="Hey, join @OS_Weekends and start contributing now! 🎃💙"
-     data-url="https://github.com/OSWeekends/osw-hacktoberfest-2019" data-hashtags="Hacktoberfest"
+  <a href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+     class="twitter-share-button"
+     data-size="large"
+     :data-text="twObj.text"
+     :data-url="twObj.url"
+     :data-hashtags="twObj.hashTag"
      data-show-count="false"></a>
 </template>
 
 <script>
+const twObj = {
+  text: 'Hey!, join @OS_Weekends and start contributing now! 🎃💙',
+  hashTag: 'Hacktoberfest, OSW_Hacktoberfest_19',
+  url: 'https://github.com/OSWeekends/osw-hacktoberfest-2019'
+}
+
 export default {
-  name: 'ShareTwitterBtn'
+  name: 'ShareTwitterBtn',
+  data () {
+    return {
+      twObj
+    }
+  }
 }
 </script>

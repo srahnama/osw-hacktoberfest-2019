e2d774b8266f4f95f4e8dc418e410dbf09218a33
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-10-03 20:16:15 +0200

    WIP

--- src/views/Stats/Index.vue
+++ src/views/Stats/Index.vue
@@ -9,10 +9,16 @@
     <template v-else>
       <p>Loading...</p>
     </template>
+
+    <template v-if="isReady">
+      <apexchart height="200" :options="options" :series="series"></apexchart>
+    </template>
   </div>
 </template>
 
 <script>
+import { get } from 'axios'
+
 import HeaderTitle from '@/components/HeaderTitle'
 import contributors from '@/assets/contributors.json'
 import StatsWidget from './StatsWidget'
@@ -29,11 +35,43 @@ export default {
     colorHelper,
     loaded: false,
     averageColor: undefined,
-    contributorsCount: contributors.length.toString()
+    contributorsCount: contributors.length.toString(),
+    isReady: false,
+    selectedDay: 1,
+    selectedData: [],
+    options: {
+      chart: {
+        width: '100%',
+        height: 200,
+        type: 'bar',
+        id: 'example'
+      }
+    },
+    series: [
+      {
+        name: 'Number of Commits'
+      }
+    ]
   }),
   async mounted () {
     this.averageColor = await this.getRGBAverage()
     this.loaded = true
+
+    const url = 'https://api.github.com/repos/OSWeekends/osw-hacktoberfest-2019/stats/punch_card'
+    get(url)
+      .then((res) => {
+        console.log(res)
+        this.selectedData = res.data.filter(item => item[0] === 1)
+        console.log(this.selectedData)
+        this.options.xaxis = {
+          categories: this.selectedData.map(item => item[1])
+        }
+        this.series[0].data = this.selectedData.map(item => item[2])
+        this.isReady = true
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   },
   methods: {
     async getRGBAverage () {

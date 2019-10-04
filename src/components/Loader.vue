b93602716b8205697ef1f373e692a5bb5b62b35a
Daniel Ormeno <dormenog@gmail.com>
2019-10-03 19:06:40 -0700

    Improvements to Stats page * Adds loader * Improves responsiveness of ColorBox

---
+++ src/components/Loader.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="center">
+    <h1>
+      <img src="https://cultofthepartyparrot.com/parrots/hd/twinsparrot.gif" />
+    </h1>
+    <h4>{{message}}</h4>
+  </section>
+</template>
+<script>
+export default {
+  name: 'Loader',
+  props: { message: { type: String, default: 'Loading...' } }
+}
+</script>

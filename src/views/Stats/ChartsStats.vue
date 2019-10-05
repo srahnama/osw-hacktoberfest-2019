2d1e2afe80e22f93ff1f25e67778cbaf6aeba839
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-10-05 23:22:05 +0200

    Stats by day/hour

--- src/views/Stats/ChartsStats.vue
+++ src/views/Stats/ChartsStats.vue
@@ -1,50 +1,66 @@
 <template>
   <div class="mt-5 pt-5">
-    <b-form-group label="Days of the week">
+
+    <hr class="mb-5">
+
+    <p class="font-weight-bold">
+      Total of commits:
+      <b-badge variant="info">{{ totalCommits }}</b-badge>
+    </p>
+
+    <b-form-group>
       <b-form-radio-group
-        v-model="selected"
+        v-model="selectedDay"
         :options="days"
         name="radio-inline"
-        @change="fetchData"
+        @input="populateCharts(selectedDay)"
       ></b-form-radio-group>
     </b-form-group>
-    <pre>{{selected}}</pre>
 
-    <apexchart v-if="isReady" height="200" :options="options" :series="series"></apexchart>
-    <p class="lead text-center">Number of commits for day in each hour</p>
+    <apexchart v-if="isReady" height="200" :options="opts" :series="series"></apexchart>
   </div>
 </template>
+
 <script>
 import { get } from 'axios'
 
 const days = [
-  { text: 'Monday', value: '1' },
-  { text: 'Tuesday', value: '2' },
-  { text: 'Wednesday', value: '3' },
-  { text: 'Thursday', value: '4' },
-  { text: 'Friday', value: '5' },
-  { text: 'Saturday', value: '6' },
-  { text: 'Sunday', value: '7' }
+  { text: 'Monday', value: 1 },
+  { text: 'Tuesday', value: 2 },
+  { text: 'Wednesday', value: 3 },
+  { text: 'Thursday', value: 4 },
+  { text: 'Friday', value: 5 },
+  { text: 'Saturday', value: 6 },
+  { text: 'Sunday', value: 0 }
 ]
+
+const options = {
+  chart: {
+    width: '100%',
+    height: 200,
+    type: 'bar',
+    id: 'example'
+  },
+  title: {
+    text: 'Number of commits for day grouped by hour',
+    align: 'left',
+    style: {
+      fontSize: '15px'
+    }
+  }
+}
 
 export default {
   name: 'ChartsStats',
   data () {
     return {
       isReady: false,
+      days,
       selectedDay: 1,
-      selectedData: [],
-      options: {
-        chart: {
-          width: '100%',
-          height: 200,
-          type: 'bar',
-          id: 'example'
-        }
-      },
-      series: [],
-      selected: '1',
-      days
+      statsRaw: [],
+      series: [{ name: 'Number of commits' }],
+      opts: {},
+      totalCommits: undefined
     }
   },
   methods: {
@@ -53,18 +69,31 @@ export default {
       const url = 'https://api.github.com/repos/OSWeekends/osw-hacktoberfest-2019/stats/punch_card'
       get(url)
         .then((res) => {
-          this.selectedData = res.data.filter(item => item[0] === Number(this.selected))
-          console.log(this.selectedData)
-          this.options.xaxis = { categories: this.selectedData.map(item => item[1]) }
-          this.series = []
-          this.series.push({})
-          this.series[0].data = this.selectedData.map(item => item[2])
-          this.series[0].name = 'Number of Commits'
+          this.statsRaw = res.data
+          this.populateCharts(this.selectedDay)
           this.isReady = true
         })
         .catch((err) => {
           console.log(err)
+          this.$bvToast.toast(`${err} 😅`, {
+            title: 'Wooops!',
+            variant: 'danger',
+            autoHideDelay: 5000
+          })
         })
+    },
+    /**
+     * Load data in the chart
+     * @param day Number
+     */
+    populateCharts (day) {
+      const values = this.statsRaw.filter(item => item[0] === day)
+      this.opts = {
+        ...options,
+        xaxis: { categories: values.map(item => item[1]) }
+      }
+      this.series[0].data = values.map(item => item[2])
+      this.totalCommits = values.map(item => item[2]).reduce((a, b) => a + b)
     }
   },
   mounted () {
